f3a308b30d5c2847440d87f91db59755ca666c37
Rachel Duffin <rachel.g.duffin@gmail.com>
2023-01-03 17:32:03 +0000

    again

--- scripts/add_last_modified_date.sh
+++ scripts/add_last_modified_date.sh
@@ -10,8 +10,9 @@ for f in $FILES; do
     if echo "$file_modified_date" | grep -q "$str"; then
       echo "File has not been modified: $f"
     else
+      datetime_now=$(date +'%Y-%m-%d %H:%M:%S')
       printf "%s modified since last pull request: %s" "$f" "$str"
-      sed -i "s/.*last_modified_date:.*/last_modified_date: $str/" "$f"
+      sed -i "s/.*last_modified_date:.*/last_modified_date: $datetime_now/" "$f"
     fi
 done
 
