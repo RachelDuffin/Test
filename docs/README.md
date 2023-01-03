417106cec984fb109f9d6025003c1ef9ed0935cf
Rachel Duffin <rachel.g.duffin@gmail.com>
2023-01-03 15:26:42 +0000

    T

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
 ---
-last_modified_date: 2023-01-03 14:50:58
+last_modified_date: 2023-01-03 15:02:55
 ---
 # Doc
--- scripts/add_last_modified_date.sh
+++ scripts/add_last_modified_date.sh
@@ -9,9 +9,16 @@ for f in $FILES; do
     fi
 done
 for f in $FILES; do
+    file_modified_date=$(sed -n "/.*last_modified_date:.*/ p" $f)
+    echo "Modified date in file: $file_modified_date"
     str="$(git log -1 --pretty=format:"%cd" --date=format:'%Y-%m-%d %H:%M:%S' $f)"
-    sed -i "s/.*last_modified_date:.*/last_modified_date: $str/" "$f"
-    printf "%-${MAXLEN}s %s\n" "$f" "$str"
+    echo "Modified date in git log: $str"
+    if [[ "$file_modified_date" =~ .*"$str".* ]]; then
+      echo "File has not been modified: $f"
+    else
+      printf "%-${MAXLEN}s %s\n File modified since last pull request:" "$f" "$str"
+      sed -i "s/.*last_modified_date:.*/last_modified_date: $str/" "$f"
+    fi
 done
 
 
